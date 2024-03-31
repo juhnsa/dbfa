38393b32648644527acaa15ecd553281bf1d57bd
Juhn-Vitus <61094247+juhnsa@users.noreply.github.com>
2024-03-31 22:15:18 +0200

    Restructure for Package Building.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dbfa',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    author='juhnsa',
+    author_email='juhnsa',
+    description='Database for the poor',
+    url='https://github.com/juhnsa/dbfa',
+)
