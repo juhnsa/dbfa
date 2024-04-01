f3cd851d06ba049369f41ad83269dd08e439a750
Juhn-Vitus <61094247+juhnsa@users.noreply.github.com>
2024-04-01 03:01:01 +0200

    Update test_dbfa.py with TODOs

--- tests/test_dbfa.py
+++ tests/test_dbfa.py
@@ -1,3 +1,11 @@
-from pytest import *
-from dbfa import dbfa
+#from pytest import *
+#from dbfa import dbfa
 
+# to do
+# Add test cases for
+# - add_entry
+# - update_entry
+# - remove_entry
+# - create_db
+# - query_db
+# - delete_db
